73a55b939eff4e0a2d11d6fd1f1c82db5377c3bc
lbouayad <8428590+lbouayad@users.noreply.github.com>
2026-02-27 23:12:04 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,18 +3,20 @@ import Header from "./components/Abstracts/Header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Top Health AI",
-  icons: {
-    icon: "/ai.png",
-  },
+  title: "Top Health AI Studio",
   description:
-    "Join the #1 platform connecting U.S. doctors with patients overseas in need of critical second opinions.",
+    "Responsible AI Education & Research for Healthcare. Advancing clinically defensible AI through structured evaluation, prototype-based testing, and interdisciplinary collaboration.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"bg-bgWhite"}>
+      <body className="bg-bgWhite">
         <Header />
         {children}
         <Footer />
